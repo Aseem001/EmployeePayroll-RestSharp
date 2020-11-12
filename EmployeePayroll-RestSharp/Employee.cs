37afe9d6bd5ca7acaa1241b0956f193588d1af75
Aseem <aseemanand2@gmail.com>
2020-11-12 22:12:48 +0530

    [Aseem] Created new repo and added necessary files

---
+++ EmployeePayroll-RestSharp/Employee.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace EmployeePayroll_RestSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+}
